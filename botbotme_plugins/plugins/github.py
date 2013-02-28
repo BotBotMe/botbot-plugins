612686f596f99169df70e9ea5d8b16a1268c2041
Peter Baumgartner <pete@lincolnloop.com>
2013-02-28 10:58:19 -0600

    Some cleanup on the GitHub plugin

--- botbotme_plugins/plugins/github.py
+++ botbotme_plugins/plugins/github.py
@@ -14,43 +14,49 @@ Looking for the url of an issue or a list of issue:
 Note: The lookup is limited to 5 issues.
 """
 
+
 class Config(config.BaseConfig):
     organization = config.Field(help_text="GitHub organization")
     repo = config.Field(required=False,
                         help_text="GitHub repository name")
-    user = config.Field(required=False,
-                        help_text="GitHub username to connect to API (for private repos)")
-    password = config.Field(required=False,
-                            help_text="GitHub password to connect to API (for private repos)")
+    user = config.Field(
+        required=False,
+        help_text="GitHub username to connect to API (for private repos)")
+    password = config.Field(
+        required=False,
+        help_text="GitHub password to connect to API (for private repos)")
 
 
-URL = "https://api.github.com"
+URL = "https://api.github.com/repos"
+
 
 @app.route(ur'(?:.*)(?:GH|gh):(?P<repo>[\w\-\_]+)#?(?P<issues>[\d\s,]+)(?:.*)',
-               listens_to="all_messages")
+           listens_to="all_messages")
 def issue_lookup(line, repo, issues):
+    """Lookup an specified repo issue"""
     gh_config = line.plugin_config.fields
     # issues can be a list of issue separated by a comma
     issue_list = [i.strip() for i in issues.split(",")]
     response_list = []
     for issue in issue_list[:5]:
-        api_url = "/".join([URL, "repos", gh_config['organization'],
+        api_url = "/".join([URL, gh_config['organization'],
                             repo, "issues", issue])
-        if gh_config['user'] and gh_config['password']:
-            auth = (gh_config['user'], gh_config['password'])
-        else:
-            auth = None
-        response = requests.get(api_url, auth=auth)
+        response = requests.get(api_url, auth=_get_auth(gh_config))
         if response.status_code == 200:
-            response_list.append(u'{title}: {html_url}'.format(**response.json))
+            resp = u'{title}: {html_url}'.format(**response.json())
+            response_list.append(resp)
         else:
-            response_list.append(u"Sorry I couldn't find issue #{0} in {1}/{2}".format(issue, gh_config['organization'], repo))
+            resp = u"Sorry I couldn't find issue #{0} in {1}/{2}".format(
+                issue, gh_config['organization'], repo)
+            response_list.append(resp)
 
     return ", ".join(response_list)
 
+
 @app.route(ur'(?:.*)(?:GH|gh)#?(?P<issues>[\d\s,]+)(?:.*)',
-               listens_to="all_messages")
+           listens_to="all_messages")
 def project_issue_lookup(line, issues):
+    """Lookup an issue for the default repo"""
     gh_config = line.plugin_config.fields
     if not (gh_config['organization'] and gh_config['repo']):
         return
@@ -58,13 +64,21 @@ def project_issue_lookup(line, issues):
     issue_list = [i.strip() for i in issues.split(",")]
     response_list = []
     for issue in issue_list[:5]:
-        api_url = "/".join([URL, "repos", gh_config['organization'],
+        api_url = "/".join([URL, gh_config['organization'],
                             gh_config['repo'], "issues", issue])
-        response = requests.get(api_url)
+        response = requests.get(api_url, auth=_get_auth(gh_config))
         if response.status_code == 200:
-            response_list.append(u'{title}: {html_url}'.format(**response.json))
+            resp = u'{title}: {html_url}'.format(**response.json())
+            response_list.append(resp)
         else:
-            response_list.append(u"Sorry I couldn't find issue #{0} in {1}/{2}".format(issue, gh_config['organization'], repo))
+            resp = u"Sorry I couldn't find issue #{0} in {1}/{2}".format(
+                issue, gh_config['organization'], gh_config['repo'])
+            response_list.append(resp)
 
     return ", ".join(response_list)
 
+def _get_auth(gh_config):
+    """Return user credentials if they are configured"""
+    if gh_config['user'] and gh_config['password']:
+        return (gh_config['user'], gh_config['password'])
+    return None
