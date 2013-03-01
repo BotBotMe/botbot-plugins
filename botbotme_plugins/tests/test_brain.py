f5e28eb00c599595c9c6a8ff8489234df444e062
Yann Malet <yann.malet@gmail.com>
2013-03-01 15:00:01 +0100

    replaced ur'' by u''

--- botbotme_plugins/tests/test_brain.py
+++ botbotme_plugins/tests/test_brain.py
@@ -8,18 +8,17 @@ def setup_function(function):
 
 
 def test_remember():
-    responses = base.app.respond(ur"@shrug=¯\_(ツ)_/¯")
+    responses = base.app.respond(u"@shrug=¯\_(ツ)_/¯")
     assert responses == [u'I will remember "shrug" for you repl_user.']
     responses = base.app.respond(ur"@shrug ?")
     assert responses == [u"¯\_(ツ)_/¯"]
 
 
 def test_recall():
-    import pytest; pytest.set_trace()
     # TODO: understand why base.app.direct_messages_router is not set
     # {'brain': [(u'(?P<key>.+?)=\\s*(?P<value>.*)', <function remember at 0x1a2f7d0>), (u'(?P<key>.*)\\?', <function recall at 0x1a2f8c0>)]}
     assert {} != base.app.direct_messages_router
 
     base.app.storage[u'brain:shrug'] = '\xc3\x82\xc2\xaf\\_(\xc3\xa3\xc2\x83\xc2\x84)_/\xc3\x82\xc2\xaf'
-    responses = base.app.respond(ur"@shrug ?")
+    responses = base.app.respond(r"@shrug ?")
     assert responses == [u"¯\_(ツ)_/¯"]
