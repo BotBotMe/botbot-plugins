7995621967f32e13c449f3127961ba294a0c2b19
Peter Baumgartner <pete@lincolnloop.com>
2013-03-04 08:54:05 -0600

    Mocked GitHub test

--- botbotme_plugins/tests/test_github.py
+++ botbotme_plugins/tests/test_github.py
@@ -1,7 +1,16 @@
 import pytest
+from mock import patch
+import requests
 from botbotme_plugins.base import DummyApp
 from botbotme_plugins.plugins import github
 
+class FakeResponse(object):
+    """Dummy response from GitHub"""
+    status_code = 200
+    json = lambda x: {
+        'title': 'import PIL',
+        'html_url': 'https://github.com/lincolnloop/python-qrcode/issues/2'
+    }
 
 @pytest.fixture
 def app():
@@ -10,8 +19,12 @@ def app():
     return app
 
 
-# FIXME `requests.get` needs to be mocked
 def test_github(app):
-    responses = app.respond("I'm working on gh:python-qrcode#2 today")
-    expected = "import PIL: https://github.com/lincolnloop/python-qrcode/issues/2"
-    assert responses == [expected]
+    # patch requests.get so we don't need to make a real call to GitHub
+    with patch.object(requests, 'get') as mock_get:
+        mock_get.return_value = FakeResponse()
+        responses = app.respond("I'm working on gh:python-qrcode#2 today")
+        expected_url = 'https://api.github.com/repos/lincolnloop/python-qrcode/issues/2'
+        mock_get.assert_called_with(expected_url, auth=None)
+        expected = "import PIL: https://github.com/lincolnloop/python-qrcode/issues/2"
+        assert responses == [expected]
